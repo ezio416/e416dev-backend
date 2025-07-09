06bde0679dc68099ca1c4613f59d7ba82cfa2b13
Ezio <e@e416.dev>
2025-07-08 21:57:06 -0600

    u10s, club campaign warriors, add map ids to warrior tables

--- src/maintenance.py
+++ src/maintenance.py
@@ -1,6 +1,7 @@
 # c 2025-01-27
-# m 2025-04-02
+# m 2025-07-08
 
+import csv
 from datetime import timezone
 
 from nadeo_api import live
@@ -10,6 +11,78 @@ from files import *
 from github import *
 from utils import *
 from webhooks import *
+
+
+def add_club_campaign_warriors(club_id: int, campaign_id: int, factor: float = 0.5) -> None:
+    tokens = get_tokens()
+    maps = get_tops_for_club_campaign(tokens, club_id, campaign_id, factor)
+
+    with Cursor(FILE_DB) as db:
+        for i, map in enumerate(maps):
+            uid = map['uid']
+
+            db.execute(f'''
+                INSERT INTO WarriorOther (
+                    authorTime,
+                    campaignId,
+                    campaignName,
+                    clubId,
+                    clubName,
+                    mapIndex,
+                    mapUid,
+                    name,
+                    warriorTime,
+                    worldRecord,
+                    mapId
+                ) VALUES (
+                    "{map['at']}",
+                    "{map['campaign_id']}",
+                    "{map['campaign_name']}",
+                    "{map['club_id']}",
+                    "{map['club_name']}",
+                    "{i}",
+                    "{uid}",
+                    "{map['name']}",
+                    "{map['wm']}",
+                    "{map['wr']}",
+                    "{map['id']}"
+                )
+            ''')
+
+    pass
+
+
+def add_map_ids_to_warriors() -> None:
+    MAX_UIDS = 291
+    token= get_token_core()
+
+    with Cursor(FILE_DB) as db:
+        for table_table in 'Other', 'Seasonal', 'Totd', 'Weekly':
+            table_name = f'Warrior{table_table}'
+
+            maps = {}
+            for entry in db.execute(f'SELECT * FROM {table_name}').fetchall():
+                map = dict(entry)
+                maps[map['mapUid']] = map
+
+            uids = list(maps)
+            while len(uids):
+                print(f'{table_name} has {len(uids)} maps left')
+
+                uid_count = min(len(uids), MAX_UIDS)
+                uids_this_req = uids[:uid_count]
+                uids = uids[uid_count:]
+                endpoint = f'maps/?mapUidList={','.join(uids_this_req)}'
+
+                time.sleep(0.5)
+                req = core.get(token, endpoint)
+                for map in req:
+                    existing = maps[map['mapUid']]
+                    existing['mapId'] = map['mapId']
+
+            db.execute(f'ALTER TABLE {table_name} ADD mapId CHAR(36);')
+            for uid, map in maps.items():
+                db.execute(f'UPDATE {table_name} SET mapId = "{map['mapId']}" WHERE mapUid = "{map['mapUid']}"')
 
 
 def display_db_epoch_vals() -> None:
@@ -28,6 +101,62 @@ def display_db_epoch_vals() -> None:
             f'{dt.fromtimestamp(int(val), timezone.utc).strftime('%Y-%m-%d %H:%M:%Sz')}',
             f'({'in ' if diff > 0 else ''}{format_long_time(abs(diff))}{' ago' if diff <= 0 else ''})'
         )
+
+
+def get_tops_for_club_campaign(tokens: dict, club_id: int, campaign_id: int, factor: float = 0.5) -> list:
+    print(f'getting maps for club {club_id}, campaign {campaign_id}')
+
+    time.sleep(0.5)
+    req = live.get(
+        tokens['live'],
+        f'api/token/club/{club_id}/campaign/{campaign_id}'
+    )
+
+    club_name = req['clubName']
+    campaign = req['campaign']
+    campaign_name = campaign['name']
+    uids = []
+
+    for entry in campaign['playlist']:
+        uids.append(entry['mapUid'])
+
+    print(f'getting info for club {club_id}, campaign {campaign_id}')
+
+    time.sleep(0.5)
+    info = live.get(
+        tokens['live'],
+        f'api/token/map/get-multiple?mapUidList={'%2C'.join(uids)}'
+    )
+
+    maps = []
+
+    for entry in info['mapList']:
+        maps.append({
+            'at': entry['authorTime'],
+            'campaign_id': campaign_id,
+            'campaign_name': campaign_name,
+            'club_id': club_id,
+            'club_name': club_name,
+            'id': entry['mapId'],
+            'name': entry['name'],
+            'uid': entry['uid']
+        })
+
+    for map in maps:
+        uid = map['uid']
+
+        print(f'getting top for map {uid}')
+
+        time.sleep(0.5)
+        top = live.get(
+            tokens['live'],
+            f'api/token/leaderboard/group/Personal_Best/map/{uid}/top'
+        )
+
+        map['wr'] = top['tops'][0]['top'][0]['score']
+        map['wm'] = calc_warrior_time(map['at'], map['wr'], factor)
+
+    return maps
 
 
 def migrate_old_warriors() -> None:
@@ -180,6 +309,87 @@ def migrate_old_warriors() -> None:
     pass
 
 
+def process_u10s() -> None:
+    def load_csv() -> dict:
+        data = {}
+
+        with open('data/u10s.csv') as f:
+            for i, line in enumerate(csv.reader(f)):
+                if not i:
+                    continue
+
+                data[line[1]] = int(float(line[3][3:]) * 1000)
+
+        return data
+
+    club_id = 18974
+    campaign_ids = (
+        50725,
+        50726,
+        51499,
+        51756,
+        53699,
+        53830,
+        54696,
+        57793,
+        62345,
+        62508,
+        63068,
+        65166,
+        72335,
+        72336,
+        72491,
+        74657,
+        78687,
+        78690,
+        78964,
+        82560
+    )
+
+    csv_data = load_csv()
+
+    tokens = get_tokens()
+
+    for id in campaign_ids:
+        maps = get_tops_for_club_campaign(tokens, club_id, id)
+
+        print(f'writing db for club {club_id}, campaign {id}')
+
+        with Cursor(FILE_DB) as db:
+            for i, map in enumerate(maps):
+                uid = map['uid']
+
+                db.execute(f'''
+                    INSERT INTO WarriorOther (
+                        authorTime,
+                        campaignId,
+                        campaignName,
+                        clubId,
+                        clubName,
+                        mapIndex,
+                        mapUid,
+                        name,
+                        warriorTime,
+                        worldRecord
+                    ) VALUES (
+                        "{map['at']}",
+                        "{map['campaign_id']}",
+                        "{map['campaign_name']}",
+                        "{map['club_id']}",
+                        "{map['club_name']}",
+                        "{i}",
+                        "{uid}",
+                        "{map['name']}",
+                        "{csv_data[uid]}",
+                        "{map['wr']}"
+                    )
+                ''')
+
+        pass
+
+    pass
+
+
 def test_club_campaign_error() -> None:
     tokens = get_tokens()
 
@@ -216,6 +426,13 @@ if __name__ == '__main__':
     # to_github_old()
     # test_club_campaign_error()
     # print(webhook_seasonal(None))
-    # print(calc_warrior_time(52382, 51255, 0.125))
+    # print(calc_warrior_time(25019, 23385, 0.65))  # sp25-01
+    # print(calc_warrior_time(27472, 25798, 0.55))  # sp25-06
+    # print(calc_warrior_time(37288, 34724, 0.45))  # sp25-11
+    # print(calc_warrior_time(42927, 40006, 0.35))  # sp25-16
+    # print(calc_warrior_time(20000, 16426, 0.5))
+    # process_u10s()
+    # add_club_campaign_warriors(9, 35357)  # openplanet school
+    # add_map_ids_to_warriors()
 
     pass
